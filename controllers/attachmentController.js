6d8b14d3e9091e325f767b82fb12bdd7e6898059
frendikapratama <frendikapratama28@gmail.com>
2025-12-12 15:44:41 +0700

    atachment realtime

--- controllers/attachmentController.js
+++ controllers/attachmentController.js
@@ -66,10 +66,13 @@ export async function addAttachment(req, res) {
 
     // Create notification dan emit socket untuk semua PIC (kecuali yang upload)
     if (task.pic && task.pic.length > 0) {
-      for (const picId of task.pic) {
-        if (picId.toString() !== req.user._id.toString()) {
+      for (const pic of task.pic) {
+        // Ekstrak ID yang benar dari object atau string
+        const picUserId = pic._id ? pic._id.toString() : pic.toString();
+
+        if (picUserId !== req.user._id.toString()) {
           await createAttachmentNotification({
-            recipientId: picId,
+            recipientId: picUserId, // Gunakan ID yang sudah diekstrak
             senderId: req.user._id,
             taskId: task._id,
             taskName: task.nama,
@@ -82,7 +85,8 @@ export async function addAttachment(req, res) {
 
           // Emit real-time notification via Socket.IO
           if (io) {
-            io.to(`user:${picId}`).emit("notification:attachment", {
+            io.to(`user:${picUserId}`).emit("notification:attachment", {
+              // Gunakan picUserId
               type: "TASK_ATTACHMENT_UPLOADED",
               title: "File Uploaded to Task",
               message: `${sender.username} uploaded a file "${req.file.originalname}" to "${task.nama}"`,
