441c49c027b1151b8628ea76147283ab493b306e
frendikapratama <frendikapratama28@gmail.com>
2026-09-17 10:53:02 +0700

    baileys

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ startTaskOverdueNotificationJob(io);
 startMeetingReminderJob();
 updateMeetingStatuses(io);
 startReplyListener();
-initWhatsApp();
+// initWhatsApp();
 setInterval(() => updateMeetingStatuses(io), 60 * 1000);
 export default app;
 export { io };
